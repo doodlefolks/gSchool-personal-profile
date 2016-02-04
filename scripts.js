6a956d6ca63ba5425115116cddd0c0656f12efc6
doodlefolks <doodlefolks@gmail.com>
2016-02-03 21:05:59 -0800

    Update animation on header toggle
     - Function added that executes after animation to change symbol.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
   $('.accordion').click(function() {
-      $(this).next().toggle('fast');
+      var $this = $(this);
+      $this.next().toggle('fast', function () {
+        var headerText = $this.text();
+        if ($this.next().is(':hidden')) {
+          $this.text(headerText.slice(0, headerText.length - 1) + '+');
+        } else {
+          $this.text(headerText.slice(0, headerText.length - 1) + '-');
+        }
+      });
       return false;
   }).next().hide();
 });
